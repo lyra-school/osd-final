d374f28ac030341d5170edd419395390aa3550db
S00233718 <s00233718@atu.ie>
2025-03-27 17:39:20 +0000

    Added some tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { signal, WritableSignal } from '@angular/core';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -12,5 +13,19 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should update the signal', () => {
+    let currentUserId: WritableSignal<string> = service.currentUserId;
+    service.updateCurrentUserId("hello");
+    
+    expect(currentUserId()).toBe("hello");
+  });
+
+  it('should not emit default value', () => {
+    let currentUserId: WritableSignal<string> = service.currentUserId;
+    service.updateCurrentUserId("hello");
+    
+    expect(currentUserId()).not.toBe("");
   });
 });
